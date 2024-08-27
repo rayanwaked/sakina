9a2338f424102ad86bafeff679ad3f175a3b3c0e
Rayan Waked <rayan@waked.dev>
2024-08-27 14:15:10 -0700

    Initial Commit

---
+++ Sakina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sakina
+//
+//  Created by Rayan Waked on 8/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sakina/SakinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakinaApp.swift
+//  Sakina
+//
+//  Created by Rayan Waked on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SakinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
